dcaf9fc4d90b314671fa709259734414ef24edac
codesterkemp <sparky_csk@yahoo.com>
2012-12-19 18:31:24 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twit2::Application.config.secret_token = '206724ceaaa77d8c32e23fff4ddfff8bd3b9e1ef8bf030ad4f1070a15362d30abb2ae290c66b54ce7b5e22fb170cd0907fd85cc7cd314d185009bb742be81642'
